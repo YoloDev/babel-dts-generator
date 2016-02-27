e0948df3171afdaeed8b8b754d655464fc6b31f5
Mike Graham <mgraham@technicalmedia.com>
2016-02-27 05:09:43 -0700

    failing tests created for commented class members

---
+++ spec/comments/comment-class-method.expected.ts
@@ -0,0 +1,11 @@
+export class Foo {
+  /**
+  * This is a Foo constructor
+  */
+  constructor(a:string, b:number);
+  
+  /**
+  * This is a Foo method
+  */
+  fooMethod(a:string, b:number): number;
+}
---
+++ spec/comments/comment-class-static-method.src.js
@@ -0,0 +1,11 @@
+export class Foo {
+  /**
+  * This is a Foo constructor
+  */
+  constructor(a:string, b:number) {}
+  
+  /**
+  * This is a static Foo method
+  */
+  static fooMethod(a:string, b:number): number { return 7; }
+}
